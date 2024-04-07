7ff475ee5d71ec78b73fb5afdf7e19a9cf91d496
aritra <aritrarick2002@gmail.com>
2024-04-07 18:50:48 +0530

    Initial Commit

---
+++ Quizzo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quizzo
+//
+//  Created by Aritra on 07/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quizzo/QuizzoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzoApp.swift
+//  Quizzo
+//
+//  Created by Aritra on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizzoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
