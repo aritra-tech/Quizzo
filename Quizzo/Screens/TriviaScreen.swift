357ff4c702b131d350b36601f92e9e1df831e490
aritra <aritrarick2002@gmail.com>
2024-04-08 20:12:23 +0530

    Impl navigation linking process

---
+++ Quizzo/Screens/TriviaScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaScreen.swift
+//  Quizzo
+//
+//  Created by Aritra on 08/04/24.
+//
+
+import SwiftUI
+
+struct TriviaScreen: View {
+    var body: some View {
+        QuestionScreen()
+    }
+}
+
+#Preview {
+    TriviaScreen()
+}
--- Quizzo/Screens/WelcomeScreen.swift
+++ Quizzo/Screens/WelcomeScreen.swift
@@ -9,29 +9,34 @@ import SwiftUI
 
 struct WelcomeScreen: View {
     var body: some View {
-        VStack(spacing: 40) {
-            VStack(spacing: 30) {
-                Text("Quizzo")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundStyle(Color(.black))
-                
-                Text("Welcome to Quizzo")
-                    .font(.title2)
-                    .fontWeight(.medium)
-                
-                Text("Are you ready to text out your knowledge and learn some important stuffs")
-                    .font(.subheadline)
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.regular)
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 30) {
+                    Text("Quizzo")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color(.black))
+                    
+                    Text("Welcome to Quizzo")
+                        .font(.title2)
+                        .fontWeight(.medium)
+                    
+                    Text("Are you ready to text out your knowledge and learn some important stuffs")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.regular)
+                }
+                NavigationLink {
+                    TriviaScreen()
+                } label: {
+                    PrimaryButton(text: "Let's get started!")
+                }
             }
             .padding()
-            
-            PrimaryButton(text: "Let's get started!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            .background(.white)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .edgesIgnoringSafeArea(.all)
-        .background(.white)
     }
 }
 
