2847830958f965aac44d13427b6fd43550a7d9f8
aritra <aritrarick2002@gmail.com>
2024-04-09 12:22:48 +0530

    Ceated Network Manager and other model classes

---
+++ Quizzo/Model/Trivia.swift
@@ -0,0 +1,48 @@
+//
+//  Trivia.swift
+//  Quizzo
+//
+//  Created by Aritra on 08/04/24.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        var id: UUID {
+            UUID()
+        }
+        var category: String
+        var type: String
+        var difficulty: String
+        var question: String
+        var correctAnswer: String
+        var incorrectAnswer: [String]
+        
+        var formattedQuestion: AttributedString {
+            do {
+                return try AttributedString(markdown: question)
+            } catch {
+                print("Error setting formatedQuestion: \(error)")
+                return ""
+            }
+        }
+        
+        var answer: [Answer] {
+            do {
+                let correct = [Answer(text: try AttributedString(markdown: correctAnswer), isCorrect: true)]
+                let incorrect = try incorrectAnswer.map { answer in
+                    Answer(text: try AttributedString(markdown: answer), isCorrect: false)
+                }
+                let allAnswers = correct + incorrect
+                
+                return allAnswers.shuffled()
+            } catch {
+                print("Error getting Answer: \(error)")
+                return []
+            }
+        }
+    }
+}
---
+++ Quizzo/Network/QuizzoManager.swift
@@ -0,0 +1,81 @@
+//
+//  QuizzoManager.swift
+//  Quizzo
+//
+//  Created by Aritra on 08/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+class QuizzoManager: ObservableObject {
+    
+    private(set) var trivia: [Trivia.Result] = []
+    @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.0
+    @Published private(set) var score = 0
+    
+    init() {
+        Task.init {
+            await fetchQuestion()
+        }
+    }
+    
+    func fetchQuestion() async {
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10")
+        else { fatalError("Missing URL") }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                fatalError("Error while fetching data")
+            }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(Trivia.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.trivia = decodedData.results
+                self.length = self.trivia.count
+                self.setQuestion()
+            }
+        } catch {
+            print("Error while fetching questions: \(error)")
+        }
+    }
+    
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            setQuestion()
+        } else {
+            reachedEnd = true
+        }
+    }
+    
+    func setQuestion() {
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length) * 350)
+        
+        if index < length {
+            let currentQuestion = trivia[index]
+            question = currentQuestion.formattedQuestion
+            answerChoices = currentQuestion.answer
+        }
+    }
+    
+    func selectedAnswer(answer: Answer) {
+        answerSelected = true
+        if answer.isCorrect {
+            score += 1
+        }
+    }
+}
